e30a63219d23381bd0829eff6a3bdf85e039cb5d
Krystian Sudoł <Krystian.19_9@wp.pl>
2025-11-06 14:10:18 +0100

    fixture

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,23 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def test_valid_login():
-    driver = webdriver.Chrome()
+def test_valid_login(driver):
 
-    try:
-        driver.get("https://www.saucedemo.com/")
-        time.sleep(2)
-        username_field = driver.find_element(By.ID, "user-name")
-        username_field.send_keys("standard_user")
-        time.sleep(2)
-        password_field = driver.find_element(By.ID, "password")
-        password_field.send_keys("secret_sauce")
-        time.sleep(2)
-        login_button = driver.find_element(By.ID, "login-button")
-        login_button.click()
-        time.sleep(2)
-        assert "inventory.html" in driver.current_url
-    finally:
-        driver.quit()
-
-test_valid_login()
+    driver.get("https://www.saucedemo.com/")
+    time.sleep(1)
+    username_field = driver.find_element(By.ID, "user-name")
+    username_field.send_keys("standard_user")
+    time.sleep(1)
+    password_field = driver.find_element(By.ID, "password")
+    password_field.send_keys("secret_sauce")
+    time.sleep(1)
+    login_button = driver.find_element(By.ID, "login-button")
+    login_button.click()
+    time.sleep(1)
+    assert "inventory.html" in driver.current_url
+    
