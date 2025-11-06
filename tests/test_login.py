27a516cf89f50bdac60c762b737d987dada65d02
Krystian Sudoł <Krystian.19_9@wp.pl>
2025-11-06 13:37:46 +0100

    valid login test

---
+++ tests/test_login.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_valid_login():
+    driver = webdriver.Chrome()
+
+    try:
+        driver.get("https://www.saucedemo.com/")
+        time.sleep(2)
+        username_field = driver.find_element(By.ID, "user-name")
+        username_field.send_keys("standard_user")
+        time.sleep(2)
+        password_field = driver.find_element(By.ID, "password")
+        password_field.send_keys("secret_sauce")
+        time.sleep(2)
+        login_button = driver.find_element(By.ID, "login-button")
+        login_button.click()
+        time.sleep(2)
+        assert "inventory.html" in driver.current_url
+    finally:
+        driver.quit()
+
+test_valid_login()
